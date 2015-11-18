2b058fb61f97a4c52be82eed569c778b8d912767
Sagar <narikenabilli@gmail.com>
2015-11-18 23:47:20 +0530

    Removed sourceforge links from footer

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,45 +1,19 @@
 
-
-<br>
-<br>
-<?php
-	// Add some function only active on the 
-	// "php-addressbook.sourceforge.net" Demopage.
-if ($_SERVER['SERVER_NAME'] == "php-addressbook.sourceforge.net") { ?>
 <center>
 </center>
-<script type="text/javascript">
-	// Google-Analytics integration
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-	var pageTracker = _gat._getTracker("UA-6220233-1");
-	pageTracker._trackPageview();
-</script>        
-</html> 
-<?php } else {?>
+
+       
+</body> 
+
 	</div>
 	<div id="footer">
-<?php } ?>
 
-<?php
 
-	// Add some function only active on the 
-	// "php-addressbook.sourceforge.net" Demopage.
-if($_SERVER['SERVER_NAME'] == "php-addressbook.sourceforge.net") { ?>
-	<div class="right">
-		<div id="download">
-			<a href="../download"><b>Download</b></a> <?php echo "v$version"; ?>
-		</div><br class="clear" />
-	</div>
-<?php } ?>
-			<ul>
-				<li><a href="http://sourceforge.net/projects/php-addressbook/">php-addressbook</a> <a href="notes.htm">v<?php echo $version; ?></a></li>
-			</ul>
+
+			
 	</div>
 </div>
-	</body>
+	</html>
 
 <!-- 
 Copyright Notice:
